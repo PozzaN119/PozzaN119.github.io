c19d7c77a8dced336561a942d5f89356ea4c0005
NPozza119 <noe.pozza@gmail.com>
2021-06-07 19:38:48 +0200

    Create picture.js

---
+++ JS/picture.js
@@ -0,0 +1,18 @@
+
+
+function getPicture() {
+
+  /* Inisialize variable and object */
+  const requestURL = 'https://v1.nocodeapi.com/noway19/instagram/jTWDCoPAteKUQGbB?';
+  var request = new XMLHttpRequest();
+
+  /* Get JSON file */
+  request.open('GET', requestURL);
+  request.responseType = 'json';
+  request.send();
+
+  request.onload = function() {
+    const jsonpicture = request.response;
+    document.getElementById("imagedetest").src = jsonpicture['data']['0']['media_url'];
+  }
+}
